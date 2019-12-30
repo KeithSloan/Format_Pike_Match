b3e825decdc017a413e1476b570131177e537515
KeithSloan <keith@sloan-home.co.uk>
2019-12-30 20:11:48 +0000

    First

---
+++ format.py
@@ -0,0 +1,16 @@
+import sys
+
+fr=open(sys.argv[1],"r")
+fw=open("output","w")
+lines =fr.readlines()
+#print(len(lines))
+#print(lines)
+for l in lines :
+    words = l.split()
+    if words[0] == 'Chr' :
+       chrome = words[1]
+       start  = words[12]
+       end    = words[15]
+       print(chrome+' '+start+' '+end)
+       print(chrome+' '+start+' '+end, file = fw)
+fw.close()
